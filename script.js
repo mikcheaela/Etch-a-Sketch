1c997f63f6b6da152078c953bd9b7bbfe0171257
mikcheaela <170772727+mikcheaela@users.noreply.github.com>
2024-06-29 19:05:08 -0400

    Add "new grid" button and functionality to it
    Added a plus icon that prompts the user asking for
    a new grid size. A new grid is created that uses
    the same amount of pixels as the base 16x16 grid.

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
 const container = document.querySelector(".container");
 const reset = document.querySelector(".reset");
+const grid = document.querySelector(".new-grid");
+const root = document.querySelector(":root");
+const containerWidth = 720;
+const containerHeight = 784; 
+let currentSize = 16;
 
-function createGrid(){
+console.log(containerWidth);
 
-    for (let i = 0; i < 16; i++){
+function colorSquare(){
+
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((elem) => {
+        elem.addEventListener("mouseover", () => {
+            const squares = document.querySelectorAll(".square");
+            elem.style.backgroundColor = "black";
+        })
+    })
+
+}
+
+function emptyGrid(size){
+
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    createGrid(size);
+
+}
+
+function styleSquare(){
+    const rows = document.querySelectorAll(".row");
+    const squares = document.querySelectorAll(".square");
+
+    rows.forEach((elem) => {
+        elem.style.height = (containerHeight / currentSize) + "px";
+    })
+
+    squares.forEach((elem) => {
+        elem.style.width = (containerWidth / currentSize) + "px";
+    })
+
+}
+
+function createGrid(bounds){
+
+    for (let i = 0; i < bounds; i++){
         const row = container.appendChild(document.createElement("div"));
-        for (let j = 0; j < 16; j++){
+        row.classList.add("row");
+        for (let j = 0; j < bounds; j++){
             let square = document.createElement("div");
             square.classList.add("square");
             row.appendChild(square);
         }
     }
-
+    colorSquare();
+    styleSquare();
 }
 
-createGrid();
+createGrid(16);
 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((elem) => {
-    elem.addEventListener("mouseover", () => {
-        elem.style.backgroundColor = "black";
-    })
+grid.addEventListener("click", () => {
+    let size = prompt("What dimension would you like your grid to be? (Less than 100)");
+    currentSize = size;
+    emptyGrid(currentSize);
 })
 
 reset.addEventListener("click", () => {
-    squares.forEach((elem => {
-        elem.style.backgroundColor = "white";
-    }))
+    emptyGrid(currentSize);
 })
+
+
+
